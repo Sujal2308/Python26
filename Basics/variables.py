3b3017fc8795126f371e5e49644bca603cc9e1ad
Sujal Bhugul <sujalbhugul08@gmail.com>
2026-06-15 00:21:05 +0530

    Add initial variables.py file with examples and guidelines for variable usage in Python

---
+++ Basics/variables.py
@@ -0,0 +1,30 @@
+# Variables in Python
+# A variable is a container for storing data values. In Python, you don't need to declare the type of variable, it is determined automatically based on the value assigned to it.
+
+# Example of variable assignment
+x = 5
+print(x)
+y = "Hello, World!"
+print(y)
+# You can also assign multiple variables in one line
+a, b, c = 1, 2, 3
+print(a, b, c)
+# Variables can be of different types
+num = 10
+print(num)
+pi = 3.14
+print(pi)
+is_valid = True
+print(is_valid)
+# You can also reassign variables to different types
+x = "Now I'm a string"
+print(x)
+x = 42
+print(x)    
+
+# variable names should be descriptive and follow certain rules:
+# - Variable names must start with a letter or an underscore (_).   
+# - Variable names cannot start with a number.
+# - Variable names can only contain alphanumeric characters and underscores (A-z, 0-9, and _).
+# follow snake_case convention for variable names in Python
+my_variable = 10
