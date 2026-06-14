0c4e6adf3aa60312bc083238dd157346e6b4ec73
Sujal Bhugul <sujalbhugul08@gmail.com>
2026-06-15 00:24:34 +0530

    Add conditionals.py with examples of if, elif, and else statements

---
+++ Basics/conditionals.py
@@ -0,0 +1,24 @@
+# Conditionals in Python
+# Conditionals are used to perform different actions based on different conditions. In Python, we use if, elif, and else statements to create conditional statements.
+
+#! Indentation is very important in Python. It is used to define the scope of loops, functions, and classes. In conditionals, the code block that follows the if, elif, or else statement must be indented.
+
+# Example of if statement
+x = 10
+if x > 5:
+    print("x is greater than 5")    
+# Example of if-else statement
+y = 3
+if y > 5:
+    print("y is greater than 5")
+else:
+    print("y is not greater than 5")
+
+# Example of if-elif-else statement
+z = 7
+if z > 10:
+    print("z is greater than 10")
+elif z > 5:
+    print("z is greater than 5 but less than or equal to 10")
+else:
+    print("z is less than or equal to 5")
